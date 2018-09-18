cd006bc3e91392664d9e92b1e325bf37e01115e3
Björn Þorláksson <bjossith@Macintosh-3.local>
2018-09-18 10:00:36 +0000

    added algorithm

---
+++ Tile_travel.py
@@ -0,0 +1,15 @@
+# Player starts at 1,1 and can only move north
+# We tell the player what directions he can move to
+# If he enters a direction which is not available we print not a valid direction and let him try again
+# PLayer wins when he gets to the tile 3,1 and we print out Victory!
+
+#Directions the player can move to in each tile:
+# 1,1 -> N
+# 1,2 -> N,E,S
+# 1,3 -> E,S
+# 2,1 -> N
+# 2,2 -> W, S
+# 2,3 -> E, W
+# 3,1 -> Victory!
+# 3,2 -> N,S
+# 3,3 -> W,S
